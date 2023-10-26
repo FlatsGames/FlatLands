e6e2e6be3a94a91524501dcd860bbc33ce31ea7a
Anastasia <petrovaanastasia2712@yandex.ru>
2023-10-26 21:45:36 +0300

    Creat Items Configs And Data

--- Assets/Game/Modules/Items/Code/ItemConfig.cs
+++ Assets/Game/Modules/Items/Code/ItemConfig.cs
@@ -8,8 +8,20 @@ namespace FlatLands.Items
         fileName = nameof(ItemConfig))]
     public sealed class ItemConfig : MultitonScriptableObjectsByName<ItemConfig>
     {
-       
+        [SerializeField] 
+        private ItemView _itemPrefab;
+        public ItemView ItemPrefab => _itemPrefab; 
+    
+        [SerializeField]
+        private Sprite _icon;
+        public Sprite Icon => _icon;
+
+        [SerializeField] 
+        private int _maxCount;
+        public int MaxCount => _maxCount;
+
+        [SerializeField, TextArea(5, 10)] 
+        private string _description;
+        public string Description => _description;
     }
-    
-    
 }
--- Assets/Game/Modules/Items/Code/ItemsLocationData.cs
+++ Assets/Game/Modules/Items/Code/ItemsLocationData.cs
@@ -16,5 +16,4 @@ namespace FlatLands.Items
             
         }
     }
-
 }
