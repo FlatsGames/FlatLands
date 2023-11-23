b3c4df9b4ece713ef514b66450b0c0581aa3fdef
Vladislav Safronov <safronovsteam17@gmail.com>
2023-11-23 20:36:26 +0300

    Created ui utils, fixed hud

---
+++ Assets/Architecture/Code/Utils/UIUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine.UI;
+
+namespace FlatLands.Architecture
+{
+    public static class UIUtils
+    {
+        public static void DoValue(float startValue, float endValue, float duration, Action<float> onUpdate = null, Action onCompleted = null)
+        {
+            var curValue = startValue;
+            DOTween.To(() => curValue, setter => curValue = setter, endValue, duration)
+                .OnUpdate(() =>
+                {
+                    onUpdate?.Invoke(curValue);
+                })
+                .OnComplete(() =>
+                {
+                    onCompleted?.Invoke();
+                });
+        }
+        
+        public static Tweener DoValue(this Slider slider, float toValue, float duration)
+        {
+            var curValue = slider.value;
+            return DOTween
+                .To(() => curValue, setter => curValue = setter, toValue, duration)
+                .OnUpdate((() => slider.value = curValue));
+        }
+    }
+}
--- Assets/Game/Modules/CharacterModules/CharacterAttributes/Code/UI/UIAttributeRegeneratedHudElement.cs
+++ Assets/Game/Modules/CharacterModules/CharacterAttributes/Code/UI/UIAttributeRegeneratedHudElement.cs
@@ -1,4 +1,5 @@
 ﻿using DG.Tweening;
+using FlatLands.Architecture;
 using FlatLands.GameAttributes;
 using FlatLands.UI;
 using UnityEngine;
@@ -31,6 +32,13 @@ namespace FlatLands.CharacterAttributes
                 _attribute.OnValueChangedDetails -= HandleValueChanged;
             
             _attribute = attributeData;
+            _mainSlider.minValue = _attribute.MinValue;
+            _mainSlider.maxValue = _attribute.MaxValue;
+            _mainSlider.value = _attribute.Value;
+            
+            _internalSlider.minValue = _attribute.MinValue;
+            _internalSlider.maxValue = _attribute.MaxValue;
+            _internalSlider.value = _attribute.Value;
             
             if (_attribute != null)
                 _attribute.OnValueChangedDetails += HandleValueChanged;
@@ -38,7 +46,49 @@ namespace FlatLands.CharacterAttributes
 
         private void HandleValueChanged(bool addValue, float prevValue, float newValue)
         {
-            
+            _sliderSequence?.Kill();
+            _sliderSequence = DOTween.Sequence();
+
+            if (addValue)
+            {
+                _internalSlider.value = 0;
+                _sliderSequence.Append(_mainSlider.DoValue(newValue, 0.3f));
+                _sliderSequence.AppendCallback(() => _internalSlider.value = 0);
+                return;
+            }
+
+            _internalSlider.value = prevValue;
+            _mainSlider.value = newValue;
+            _sliderSequence.AppendInterval(0.3f);
+            _sliderSequence.Append(_internalSlider.DoValue(newValue, 0.3f));
+            _sliderSequence.AppendCallback(() => _internalSlider.value = 0);
         }
+
+        // private float _debugValue = 100;
+        //
+        // private void Start()
+        // {
+        //     _mainSlider.minValue = 0;
+        //     _mainSlider.maxValue = 100;
+        //     _mainSlider.value = _debugValue;
+        //     
+        //     _internalSlider.minValue = 0;
+        //     _internalSlider.maxValue = 100;
+        //     _internalSlider.value = _debugValue;
+        // }
+        //
+        // [Button]
+        // private void AddValue()
+        // {
+        //     HandleValueChanged(true, _debugValue, _debugValue + 10);
+        //     _debugValue += 10;
+        // }
+        //
+        // [Button]
+        // private void RemoveValue()
+        // {
+        //     HandleValueChanged(false, _debugValue, _debugValue - 10);
+        //     _debugValue -= 10;
+        // }
     }
 }
